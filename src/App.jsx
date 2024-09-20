a703a1962df519a5d460c1092f6f39df2d960f81
zhburger <158474543+zhburger@users.noreply.github.com>
2024-09-20 23:55:53 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Avatar from './Components/Avatar.jsx'
+function App() {
+  
+
+  return (
+   
+    <div className="App">
+    <Avatar />
+    </div>
+  )
+}
+
+export default App
